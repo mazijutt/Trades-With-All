a0dadc70bead5a524f4c4d37121fac05bcb45d61
mazijutt <mazijutt781@gmail.com>
2026-09-13 20:20:39 +0530

    Update api.js

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,17 +1,24 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: 'https://trades-with-all-1.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 api.interceptors.response.use(
   (response) => response,
@@ -19,8 +26,10 @@ api.interceptors.response.use(
     if (error.response?.status === 401) {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
+
       window.location.href = '/login';
     }
+
     return Promise.reject(error);
   }
 );
